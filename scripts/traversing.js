f10fac7b753f4f851bd4ff69b251c4e29cac788e
mniberyl <mniberyl@gmail.com>
2020-05-12 03:26:45 +0900

    Add addBack function

--- scripts/traversing.js
+++ scripts/traversing.js
@@ -1,10 +1,21 @@
-// 기존 요소에 새로운 요소를 추가하는 함수
+// 현재 요소에 새로 전달 받은 요소를 추가하는 함수
 function add(nodes, selector) {
   const colors = "blue";
   for (let i = 0; i < nodes.length; i++) {
     nodes[i].style.color = colors;
-    for (let i = 0; i < selector.length; i++) {
-      selector[i].style.color = colors;
+    for (let j = 0; j < selector.length; j++) {
+      selector[j].style.color = colors;
     }
+  }
+}
+
+// 현재 요소의 이전 요소를 찾아 효과를 추가하는 함수
+function addBack(nodes) {
+  const border = "1px solid #FFB2D9";
+  for (i = 0; i < nodes.length; i++) {
+    nodes[i].style.border = border;
+    const parent = nodes[i].parentElement;
+    parent.style.border = border;
+    console.log(parent);
   }
 }
