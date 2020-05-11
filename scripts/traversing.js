e033f36d0b841a4d4951f21a9096b0915e54ea42
mniberyl <mniberyl@gmail.com>
2020-05-11 22:23:58 +0900

    Add add function, Fix typo in README.md

--- scripts/traversing.js
+++ scripts/traversing.js
@@ -1,3 +1,10 @@
+// 기존 요소에 새로운 요소를 추가하는 함수
 function add(nodes, selector) {
-  // @todo
+  const colors = "blue";
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].style.color = colors;
+    for (let i = 0; i < selector.length; i++) {
+      selector[i].style.color = colors;
+    }
+  }
 }
