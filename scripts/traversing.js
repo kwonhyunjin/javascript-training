e57c7c71a9855873d089c492bdc8d3c9e6707e56
mniberyl <mniberyl@gmail.com>
2020-05-13 18:27:25 +0900

    Add children function

--- scripts/traversing.js
+++ scripts/traversing.js
@@ -1,21 +1,22 @@
-// 현재 요소에 새로 전달 받은 요소를 추가하는 함수
+// 현재 선택된 요소에 새로 전달 받은 요소를 추가하는 함수(새로운 요소를 추가하는 것이 아님)
 function add(nodes, selector) {
   const colors = "blue";
   for (let i = 0; i < nodes.length; i++) {
     nodes[i].style.color = colors;
+    console.log(nodes[i]);
     for (let j = 0; j < selector.length; j++) {
       selector[j].style.color = colors;
+      console.log(selector[j]);
     }
   }
 }
 
-// 현재 요소의 이전 요소를 찾아 효과를 추가하는 함수
-function addBack(nodes) {
-  const border = "1px solid #FFB2D9";
-  for (i = 0; i < nodes.length; i++) {
-    nodes[i].style.border = border;
-    const parent = nodes[i].parentElement;
-    parent.style.border = border;
-    console.log(parent);
+// 전달 받은 요소의 자식 요소를 선택하는 함수
+function children(nodes) {
+  for (let i = 0; i < nodes.length; i++) {
+    const children = nodes[i].children;
+    for (let j = 0; j < children.length; j++) {
+      console.log(children[j]);
+    }
   }
 }
