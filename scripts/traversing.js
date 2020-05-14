d0bed39302a2366aa548ca5c13632f38d2f39bc0
mniberyl <mniberyl@gmail.com>
2020-05-14 19:26:27 +0900

    Fix children function

--- scripts/traversing.js
+++ scripts/traversing.js
@@ -1,22 +1,26 @@
 // 현재 선택한 요소에 전달 받은 요소를 추가하는 함수
 function add(nodes, selector) {
   return [...nodes, ...document.querySelectorAll(selector)];
+  // const arrayNodes = Array.from(nodes);
+  // const arraySelector = Array.from(elems);
+  // const arrayAdd = arrayNodes.concat(arraySelector);
+  // return arrayAdd;
 }
 
 // 전달 받은 요소의 자식 요소를 선택하는 함수
 function children(nodes) {
+  const newNodes = [];
   for (let i = 0; i < nodes.length; i++) {
     const children = nodes[i].children;
-    for (let j = 0; j < children.length; j++) {
-      console.log(children[j]);
-    }
+    newNodes.push(children);
   }
+  return newNodes;
 }
 
 // 선택된 요소에서 DOM 트리따라 올라가면서 최초 부모 요소를 반환하는 함수
 function closest(nodes, selector) {
   const newNodes = new Set();
-  for (i = 0; i < nodes.length; i++) {
+  for (let i = 0; i < nodes.length; i++) {
     console.log(nodes[i]);
     const node = nodes[i].closest(selector);
     console.log(node);
