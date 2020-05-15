5672fcb57845cbb8ace0fa47f2fc24030b23ca88
mniberyl <mniberyl@gmail.com>
2020-05-15 17:58:41 +0900

    Add even function

--- scripts/traversing.js
+++ scripts/traversing.js
@@ -1,4 +1,4 @@
-// 현재 선택한 요소에 전달 받은 요소를 추가하는 함수
+// 선택한 요소에 전달 받은 요소를 추가로 선택하는 함수
 function add(nodes, selector) {
   return [...nodes, ...document.querySelectorAll(selector)];
   // const arrayNodes = Array.from(nodes);
@@ -17,7 +17,7 @@ function children(nodes) {
   return newElements;
 }
 
-// 선택된 요소에서 DOM 트리따라 올라가면서 최초 부모 요소를 반환하는 함수
+// 선택한 요소부터 DOM 트리따라 올라가며 최초 부모 요소를 검색하는 함수
 function closest(nodes, selector) {
   const newElements = new Set();
   for (let i = 0; i < nodes.length; i++) {
@@ -37,13 +37,25 @@ function contents(nodes) {
   return newNodes;
 }
 
-// 선택한 요소 중에서 전달 받은 인덱스에 해당하는 요소를 반환하는 함수
+// 선택한 요소 중에서 전달 받은 인덱스에 해당하는 요소 선택하는 함수
 function eq(nodes, index) {
   const newElement = [];
   if (index < 0) {
     index += nodes.length;
   }
-  const item = nodes[index];
-  newElement.push(item);
+  const eqIndex = nodes[index];
+  newElement.push(eqIndex);
   return newElement;
+}
+
+// 선택한 요소들 중에서 짝수 인덱스를 선택하는 함수
+function even(nodes) {
+  const newNodes = [];
+  for (let i = 0; i < nodes.length; i++) {
+    if (i % 2 == 0) {
+      const evenNodes = nodes[i];
+      newNodes.push(evenNodes);
+    }
+  }
+  return newNodes;
 }
