70ebd7e6b13fa40f26329f0e001d3016fea4ae0b
Beryl <mniberyl@gmail.com>
2020-05-24 21:09:38 +0900

    Rename variables

--- scripts/collectionManipulation.js
+++ scripts/collectionManipulation.js
@@ -1,6 +1,6 @@
 // 전달 받은 엘리먼트들의 개수만큼 콜백 함수를 반복 실행하는 함수
-function each(nodes, callback) {
-  for (let i = 0; i < nodes.length; i++) {
-    callback.call(nodes[i], i);
+function each(elems, callback) {
+  for (let i = 0; i < elems.length; i++) {
+    callback.call(elems[i], i);
   }
 }
--- scripts/traversing.js
+++ scripts/traversing.js
@@ -1,201 +1,175 @@
 // 선택한 요소에 전달 받은 요소를 추가로 선택하는 함수
-function add(nodes, selector) {
-  return [...nodes, ...document.querySelectorAll(selector)];
+function add(elems, selector) {
+  return new Set([...elems, ...document.querySelectorAll(selector)]);
 }
 
 // 전달 받은 요소의 자식 요소를 선택하는 함수
-function children(nodes) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const children = nodes[i].children;
-    newElements.add(children);
+function children(elems) {
+  const newElems = [];
+  for (let i = 0; i < elems.length; i++) {
+    newElems.push(...elems[i].children);
   }
-  return newElements;
+  return new Set(newElems);
 }
 
 // 선택한 요소부터 DOM 트리따라 올라가며 최초 부모 요소를 검색하는 함수
-function closest(nodes, selector) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const node = nodes[i].closest(selector);
-    newElements.add(node);
+function closest(elems, selector) {
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i++) {
+    newElems.add(elems[i].closest(selector));
   }
-  return [...newElements];
+  return newElems;
 }
 
 // 선택된 요소의 모든 자식 요소들을 반환하는 함수
-function contents(nodes) {
-  const newNodes = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const childNodes = nodes[i].childNodes;
-    newNodes.add(childNodes);
+function contents(elems) {
+  const newNodes = [];
+  for (let i = 0; i < elems.length; i++) {
+    newNodes.push(...elems[i].childNodes);
   }
-  return newNodes;
+  return new Set(newElems);
 }
 
 // 선택한 요소 중에서 전달 받은 인덱스에 해당하는 요소 선택하는 함수
-function eq(nodes, index) {
-  const newElement = new Set();
+function eq(elems, index) {
+  const newElems = new Set();
   if (index < 0) {
-    index += nodes.length;
+    index += elems.length;
   }
-  const eqIndex = nodes[index];
-  newElement.add(eqIndex);
-  return newElement;
+  newElems.add(elems[index]);
+  return newElems;
 }
 
 // 선택한 요소들 중에서 짝수 인덱스를 선택하는 함수
-function even(nodes) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    if (i % 2 == 0) {
-      const evenNodes = nodes[i];
-      newElements.add(evenNodes);
-    }
+function even(elems) {
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i += 2) {
+    newElems.add(elems[i]);
   }
-  return newElements;
+  return newElems;
 }
 
 // 주어진 함수의 테스트를 통과하는 모든 요소를 모아 새로운 배열로 반환하는 함수
-function filter(nodes, callback) {
-  const arrayNodes = [...nodes];
-  const filterNodes = arrayNodes.filter(function (element, index) {
-    return callback.call(element, index);
+function filter(elems, callback) {
+  const newElems = [...elems].filter(function (elem, index) {
+    return callback.call(elem, index);
   });
-  return new Set(filterNodes);
+  return new Set(newElems);
 }
 
 // 선택한 요소의 하위 요소 중에서 전달 받은 특정 요소를 찾는 함수
-function find(nodes, selector) {
-  const newNodes = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const elems = nodes[i].querySelectorAll(selector);
-    newNodes.add(elems);
+function find(elems, selector) {
+  const newElems = [];
+  for (let i = 0; i < elems.length; i++) {
+    newElems.push(...elems[i].querySelectorAll(selector));
   }
-  return newNodes;
+  return new Set(newElems);
 }
 
 // 선택한 요소들 중에서 첫 번째 요소를 선택하는 함수
-function first(nodes) {
-  const newElement = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    if (i == 0) {
-      newElement.add(nodes[i]);
-    }
-  }
-  return newElement;
+function first(elems) {
+  return new Set([elems[0]]);
 }
 
 // 전달 받은 엘리먼트를 포함하고 있는 요소를 선택하는 함수
-function has(nodes, selector) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    if (nodes[i].querySelectorAll(selector).length > 0) {
-      newElements.add(nodes[i]);
+function has(elems, selector) {
+  const newElems = [];
+  for (let i = 0; i < elems.length; i++) {
+    const elem = elems[i];
+    if (elem.querySelectorAll(selector).length > 0) {
+      newElems.add(elem);
     }
   }
-  return newElements;
+  return new Set(newElems);
 }
 
 // 선택한 요소 중에서 전달 받은 선택자에 해당하는 요소가 하나라도 존재하면 true를 반환하는 함수
-function is(nodes, selector) {
-  const newArray = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const isResult = nodes[i].matches(selector);
-    newArray.add(isResult);
+function is(elems, selector) {
+  // @todo 다시 작업
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i++) {
+    const isResult = elems[i].matches(selector);
+    newElems.add(isResult);
   }
-  return newArray;
+  return newElems;
 }
 
 // 선택한 요소들 중에서 마지막 요소를 선택하는 함수
-function last(nodes) {
-  const newElement = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const lastNum = nodes.length - 1;
-    if (i == lastNum) {
-      newElement.add(nodes[i]);
-    }
-  }
-  return newElement;
+function last(elems) {
+  return new Set([elems[elems.length - 1]]);
 }
 
 // 선택한 요소 각각에 대하여 주어진 함수를 호출한 결과를 새로운 배열에 반환하는 함수
-function map(nodes, callback) {
-  const arrayNodes = [...nodes];
-  const mapNodes = arrayNodes.map(function (value, index, array) {
+function map(elems, callback) {
+  const newElems = [...elems].map(function (value, index) {
     return callback.call(value, index);
   });
-  return new Set(mapNodes);
+  return new Set(newElems);
 }
 
 // 선택한 요소의 다음에 위치한 형제 요소를 선택하는 함수
-function next(nodes) {
-  const newElement = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const nextNode = nodes[i].nextElementSibling;
-    newElement.add(nextNode);
+function next(elems) {
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i++) {
+    newElems.add(elems[i].nextElementSibling);
   }
-  return newElement;
+  return newElems;
 }
 
 // 선택한 요소 중에서 전달 받은 선택자에 해당하거나, 함수 호출 결과가 true인 요소를 제외한 나머지 요소를 모두 선택하는 함수
-function not(nodes, selector) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    if (!nodes[i].matches(selector)) {
-      newElements.add(nodes[i]);
+function not(elems, selector) {
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i++) {
+    const elem = elems[i];
+    if (!elem.matches(selector)) {
+      newElems.add(elem);
     }
   }
-  return newElements;
+  return newElems;
 }
 
 // 선택한 요소들 중에서 홀수 인덱스를 선택하는 함수
-function odd(nodes) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    if (i % 2 == 1) {
-      const oddNodes = nodes[i];
-      newElements.add(oddNodes);
-    }
+function odd(elems) {
+  const newElems = new Set();
+  for (let i = 1; i < elems.length; i += 2) {
+    newElems.add(elems[i]);
   }
-  return newElements;
+  return newElems;
 }
 
 // 전달 받은 부모 요소를 선택하는 함수
-function parent(nodes) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const parent = nodes[i].parentElement;
-    newElements.add(parent);
+function parent(elems) {
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i++) {
+    newElems.add(elems[i].parentElement);
   }
-  return newElements;
+  return newElems;
 }
 
 // 선택한 요소의 조상 요소를 모두 선택하는 함수
-function parents(nodes, selector) {
+function parents(elems, selector) {
   // @todo
 }
 
 // 선택한 요소의 이전에 위치한 형제 요소를 선택하는 함수
-function prev(nodes) {
-  const newElement = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const prevNode = nodes[i].previousElementSibling;
-    newElement.add(prevNode);
+function prev(elems) {
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i++) {
+    newElems.add(elems[i].previousElementSibling);
   }
-  return newElement;
+  return newElems;
 }
 
 // 선택한 요소 자신을 제외한 형제 요소를 모두 선택하는 함수
-function siblings(nodes) {
-  const newElements = new Set();
-  for (let i = 0; i < nodes.length; i++) {
-    const node = [...nodes[i].parentElement.children];
-    for (let j = 0; j < node.length; j++) {
-      if (nodes[i] !== node[j]) {
-        newElements.add(node[j]);
+function siblings(elems) {
+  const newElems = new Set();
+  for (let i = 0; i < elems.length; i++) {
+    const siblingsWithSelf = elems[i].parentElement.children;
+    for (let j = 0; j < siblingsWithSelf.length; j++) {
+      if (elems[i] !== siblingsWithSelf[j]) {
+        newElems.add(siblingsWithSelf[j]);
       }
     }
   }
-  return newElements;
+  return newElems;
 }
