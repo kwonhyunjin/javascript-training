2e536a47ca60e30075cbeb04448b7555d59d3146
Beryl <mniberyl@gmail.com>
2020-06-02 00:17:02 +0900

    Add function argument types to addClass function

--- scripts/manipulation.js
+++ scripts/manipulation.js
@@ -1,8 +1,8 @@
 // 선택한 요소에 클래스를 추가하는 함수
-function addClass(elems, classname) {
+function addClass(elems, callback) {
   const newElems = [];
   for (let i = 0; i < elems.length; i++) {
-    elems[i].classList.add(classname);
+    elems[i].classList.add(callback.call(elems[i], i));
     newElems.push(elems[i]);
   }
   return newElems;
