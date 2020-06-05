85e12cc773916fc6257fec2007486ee69ba78705
Beryl <mniberyl@gmail.com>
2020-06-05 15:50:28 +0900

    Add attr function

--- scripts/manipulation.js
+++ scripts/manipulation.js
@@ -14,6 +14,27 @@ function addClass(elems, condition) {
   return new Set(newElems);
 }
 
+// 선택한 요소 뒤에 새 요소를 추가하거나, 다른 곳에 있는 요소를 이동시키는 함수
+function after(elems, condition) {
+  // @todo
+}
+
+// 선택한 요소들 중에서 제일 처음 요소의 속성의 값을 가져오거나 하나 이상의 속성을 추가하는 함수
+function attr(elems, attributeName, condition) {
+  const newElems = [];
+  for (let i = 0; i < elems.length; i++) {
+    if (typeof condition == "string") {
+      elems[i].setAttribute(attributeName, condition);
+    } else if (typeof condition == "function") {
+      elems[i].setAttribute(attributeName, condition.call(elems[i], i));
+    } else {
+      return elems[0].getAttribute(attributeName);
+    }
+    newElems.push(elems[i]);
+  }
+  return newElems;
+}
+
 // 선택한 요소에 클래스를 제거하는 함수
 function removeClass(elems, condition) {
   const newElems = [];
